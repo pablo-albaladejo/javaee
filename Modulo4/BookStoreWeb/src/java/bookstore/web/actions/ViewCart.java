f17d4987ead8a0b4f0a75686e128d2cfd0c21ad7
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2013-12-24 17:08:15 +0000

    git-svn-id: https://subversion.assembla.com/svn/java_experto@25 daf995d5-a636-460e-bc68-660d2383aa96

---
+++ Modulo4/BookStoreWeb/src/java/bookstore/web/actions/ViewCart.java
@@ -0,0 +1,56 @@
+package bookstore.web.actions;
+
+import bookstore.logic.bean.book.IBookBean;
+import bookstore.logic.bean.cart.ICartBean;
+import bookstore.logic.bean.factory.BeanFactory;
+import bookstore.logic.service.ServiceFactory;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author palbaladejo
+ */
+
+public class ViewCart extends HttpServlet {
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        ICartBean myCart = (ICartBean)request.getSession().getAttribute("myCart");
+        
+        //if the cart object does not exits at session, it is created.
+        if(myCart == null){
+            myCart = BeanFactory.getInstance().getCartBean();
+            request.getSession().setAttribute("myCart", myCart);
+        }
+        
+        //Item addition into the Cart
+        String addItem = request.getParameter("addItem");
+        if(addItem != null){
+            IBookBean book = ServiceFactory.getInstance().getBusinessFacade().getBookByISBN(addItem);
+            if(book != null) myCart.add(book);
+        }
+        
+        //Item removal from the Cart
+        String removeItem = request.getParameter("removeItem");
+        if(removeItem != null){
+            IBookBean book = ServiceFactory.getInstance().getBusinessFacade().getBookByISBN(removeItem);
+            if(book != null) myCart.remove(book);
+        }
+
+        //view cart
+        request.getRequestDispatcher("/jsp/Cart.jsp").forward(request, response);
+    }
+}
