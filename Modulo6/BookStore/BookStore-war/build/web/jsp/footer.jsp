ca93f97206a006260338845975518c41357c63aa
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2014-02-03 10:58:45 +0000

    git-svn-id: https://subversion.assembla.com/svn/java_experto@47 daf995d5-a636-460e-bc68-660d2383aa96

---
+++ Modulo6/BookStore/BookStore-war/build/web/jsp/footer.jsp
@@ -0,0 +1,8 @@
+<%-- 
+    Document   : footer
+    Created on : 19-dic-2013, 19:30:52
+    Author     : Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
+--%>
+        <form action="./">
+            <input type="submit" value="Home"></input>
+        </form>
---
+++ Modulo6/BookStore/BookStore-war/src/java/bookstore/integration/ddbb/manager/TransactionManager.java
@@ -0,0 +1,81 @@
+package bookstore.integration.ddbb.manager;
+
+import bookstore.integration.ddbb.exception.TransactionException;
+
+/**
+ *
+ * <p>Pablo Albaladejo Mestre (pablo.albaladejo.mestre@gmail.com)</p>
+ */
+public abstract class TransactionManager {
+    
+    //DDBB SERVER
+    /**
+     *
+     */
+    public static final String DDBB_SERVER = "localhost";
+    /**
+     *
+     */
+    public static final String DDBB_PORT = "3306";
+    
+    public static final boolean transactionalDDBB  = false; 
+    
+    //DDBB USER
+    /**
+     *
+     */
+    public static final String DDBB_NAME = "libreria";
+    /**
+     *
+     */
+    public static final String DDBB_USER = "curso";
+    /**
+     *
+     */
+    public static final String DDBB_PASS = "curso";
+    
+    //ENTITIES
+    /**
+     *
+     */
+    public static final String BOOK_ENTITY = "libros";
+    public static final String AUTHOR_ENTITY = "autores";
+    
+    static private TransactionManager instance;
+    
+    /**
+     *
+     * @return
+     */
+    static public TransactionManager getInstance(){
+        if(instance == null) instance = new TransactionManagerImp();
+        return instance;
+    }
+    
+    	/**
+     *
+     * @throws TransactionException
+     */
+    public abstract void commit() throws TransactionException;
+	/**
+     *
+     * @throws TransactionException
+     */
+    public abstract void rollback() throws TransactionException;
+	/**
+     *
+     * @throws TransactionException
+     */
+    public abstract void begin() throws TransactionException;
+	/**
+     *
+     * @throws TransactionException
+     */
+    public abstract void close() throws TransactionException;
+	/**
+     *
+     * @return
+     * @throws TransactionException
+     */
+    public abstract Object getConexion() throws TransactionException;
+}
