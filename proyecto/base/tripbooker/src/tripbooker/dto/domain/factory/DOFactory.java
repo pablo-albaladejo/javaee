863cbdba85f42251d10e491fbad838b6673dbc88
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2014-04-07 06:55:00 +0000

    git-svn-id: https://subversion.assembla.com/svn/java_experto@97 daf995d5-a636-460e-bc68-660d2383aa96

---
+++ proyecto/base/tripbooker/src/tripbooker/dto/domain/factory/DOFactory.java
@@ -0,0 +1,36 @@
+package tripbooker.dto.domain.factory;
+
+import tripbooker.dto.domain.aircraft.IAircraftDO;
+import tripbooker.dto.domain.airline.IAirlineDO;
+import tripbooker.dto.domain.airport.IAirportDO;
+import tripbooker.dto.domain.booking.IBookingDO;
+import tripbooker.dto.domain.city.ICityDO;
+import tripbooker.dto.domain.country.ICountryDO;
+import tripbooker.dto.domain.flight.IFlightDO;
+import tripbooker.dto.domain.route.IRouteDO;
+
+/**
+ *
+ * @author Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
+ */
+
+public abstract class DOFactory {
+    private static DOFactory instance;
+    
+    public static synchronized DOFactory getInstance(){
+        if(instance == null) instance = new DOFactoryImp();
+        return instance;
+    }
+    
+    public abstract IFlightDO getFlightDO();
+    public abstract IAirportDO getAirportDO();
+    public abstract ICityDO getCityDO();
+    public abstract ICountryDO getCountryDO();
+    public abstract IAircraftDO getAircraftDO();
+    public abstract IAirlineDO getAirlineDO();
+    public abstract IRouteDO getRouteDO();    
+    public abstract IBookingDO getBookingDO();
+   
+}
+
+
---
+++ proyecto/base/tripbooker/src/tripbooker/dto/domain/factory/DOFactoryImp.java
@@ -0,0 +1,67 @@
+package tripbooker.dto.domain.factory;
+
+import tripbooker.dto.domain.aircraft.AircraftDOImp;
+import tripbooker.dto.domain.aircraft.IAircraftDO;
+import tripbooker.dto.domain.airline.AirlineDOImp;
+import tripbooker.dto.domain.airline.IAirlineDO;
+import tripbooker.dto.domain.airport.AirportDOImp;
+import tripbooker.dto.domain.airport.IAirportDO;
+import tripbooker.dto.domain.booking.BookingDOImp;
+import tripbooker.dto.domain.booking.IBookingDO;
+import tripbooker.dto.domain.city.CityDOImp;
+import tripbooker.dto.domain.city.ICityDO;
+import tripbooker.dto.domain.country.CountryDOImp;
+import tripbooker.dto.domain.country.ICountryDO;
+import tripbooker.dto.domain.flight.FlightDOImp;
+import tripbooker.dto.domain.flight.IFlightDO;
+import tripbooker.dto.domain.route.IRouteDO;
+import tripbooker.dto.domain.route.RouteDOImp;
+
+
+/**
+ *
+ * @author Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
+ */
+
+public class DOFactoryImp extends DOFactory{
+
+    @Override
+    public IAirlineDO getAirlineDO() {
+        return new AirlineDOImp();
+    }
+
+    @Override
+    public IAircraftDO getAircraftDO() {
+        return new AircraftDOImp();
+    }
+
+    @Override
+    public IAirportDO getAirportDO() {
+       return new AirportDOImp();
+    }
+
+    @Override
+    public IRouteDO getRouteDO() {
+        return new RouteDOImp();
+    }
+    
+    @Override
+    public ICityDO getCityDO() {
+        return new CityDOImp();
+    }
+
+    @Override
+    public ICountryDO getCountryDO() {
+        return new CountryDOImp();
+    }
+    
+    @Override
+    public IFlightDO getFlightDO() {
+        return new FlightDOImp();
+    }
+
+    @Override
+    public IBookingDO getBookingDO() {
+        return new BookingDOImp();
+    }
+}
